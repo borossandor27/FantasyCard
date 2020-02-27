6905f18362c5eed8013d99d888149872fa088c74
borossandor27 <boros.sanyi@gmail.com>
2020-02-27 22:37:52 +0100

    2.0

--- Fantasycard/Fantasycard.cs
+++ Fantasycard/Fantasycard.cs
@@ -12,21 +12,21 @@ namespace Fantasycard
         string name;
         string faction;
         string race;
-        string osztaly;
+        string kaszt;
 
         public int Power { get => power; set => power = value; }
         public string Name { get => name; set => name = value; }
         public string Faction { get => faction; set => faction = value; }
         public string Race { get => race; set => race = value; }
-        public string Osztaly { get => osztaly; set => osztaly = value; }
+        public string Kaszt { get => kaszt; set => kaszt = value; }
 
-        public Fantasycard(int power, string name, string faction, string race, string osztaly)
+        public Fantasycard(int power, string name, string faction, string race, string kaszt)
         {
             Power = power;
             Name = name;
             Faction = faction;
             Race = race;
-            Osztaly = osztaly;
+            Kaszt = kaszt;
         }
     }
 }
--- Fantasycard/Program.cs
+++ Fantasycard/Program.cs
@@ -15,7 +15,13 @@ namespace Fantasycard
         {
             Beolvas(@"..\..\fantasycard.csv");
             Feladat_03();
-            Console.WriteLine("Program vége!");
+            Feladat_04();
+            Feladat_05();
+            Feladat_06();
+            Feladat_07();
+            Feladat_08();
+            Feladat_09();
+            Console.WriteLine("\nProgram vége!");
             Console.ReadKey();
         }
         static void Beolvas(string forras)
@@ -46,7 +52,89 @@ namespace Fantasycard
         /// </summary>
         static void Feladat_03()
         {
-            Console.WriteLine($"\n3. feladat: {fantasycards.Find(a => a.Name.Equals("Leeroy")).Race}");
+            Fantasycard f = fantasycards.Find(a => a.Name.Equals("Leeroy"));
+            Console.WriteLine($"\n3. feladat: {f.Race} {f.Kaszt}");
+        }
+        /// <summary>
+        /// 4. Írd ki a legkisebb és a legnagyobb erejű lény nevét és erejét!
+        /// </summary>
+        static void Feladat_04()
+        {
+            Console.WriteLine("\n4. feladat:");
+            Fantasycard f = fantasycards.Find(a => a.Power == fantasycards.Max(b => b.Power));
+            Console.WriteLine($"\tname:\t{f.Name}");
+            Console.WriteLine($"\tpower:\t{f.Power}");
+        }
+        /// <summary>
+        /// 5. Határozza meg és írja ki a képernyőre a minta szerint, hogy mi volt 
+        ///    a neve annak dwarf (race)-nek akinek 4000 ereje(power) van.
+        /// </summary>
+        static void Feladat_05()
+        {
+            Console.WriteLine("\n5. feladat:");
+            Fantasycard f = fantasycards.Find(a => a.Power == 4000 && a.Race.Equals("Dwarf"));
+            if (f != null)
+            {
+                Console.WriteLine($"\tname:\t{f.Name}");
+            }
+            else
+            {
+                Console.WriteLine("\tNem találtam olyat, olyan dwarf (race)-t akinek 4000 ereje(power) van. ");
+            }
+        }
+        /// <summary>
+        /// 6. Határozza meg és írja ki a képernyőre a minta szerint, 
+        /// hogy mely Orc-oknak (race) van 4500-nál több ereje(power) 
+        /// akik a Laughing Skull(faction)-hoz tartoznak.
+        /// </summary>
+        static void Feladat_06()
+        {
+            Console.WriteLine("\n6. feladat:");
+            foreach (Fantasycard item in fantasycards.FindAll(a => a.Race.Equals("Orc") && a.Faction.Equals("Laughing Skull") && a.Power > 4500))
+            {
+                Console.WriteLine($"\t{item.Power}, {item.Name}, {item.Race}, {item.Kaszt}");
+            }
+        }
+        /// <summary>
+        /// 7. Határozza meg és írja ki a képernyőre, hogy hány darab 
+        /// Human, Orc, Elf és Dwarf (race) kártya létezik.
+        /// </summary>
+        static void Feladat_07()
+        {
+            Console.WriteLine("\n7. feladat:");
+            Console.WriteLine($"\tHuman:\t{fantasycards.Count(a => a.Race.Equals("Human")),3} db");
+            Console.WriteLine($"\tOrc:\t{fantasycards.Count(a => a.Race.Equals("Orc")),3} db");
+            Console.WriteLine($"\tElf:\t{fantasycards.Count(a => a.Race.Equals("Elf")),3} db");
+            Console.WriteLine($"\tDwarf:\t{fantasycards.Count(a => a.Race.Equals("Dwarf")),3} db");
+        }
+        /// <summary>
+        /// 8. Írja ki a képernyőre fajonként (race) a leggyengébb kártya nevét, faját és erejét!
+        /// </summary>
+        static void Feladat_08()
+        {
+            Console.WriteLine("\n8. feladat:");
+            foreach (string race in fantasycards.GroupBy(a => a.Race ).Select(b => new {race=b.Key}.race))
+            {
+                Fantasycard min = fantasycards.FindAll(a => a.Race.Equals(race)).OrderBy(b => b.Power).First();
+                Console.WriteLine($"\t{min.Name}, {race}: {min.Power} ");
+            }
+        }
+        /// <summary>
+        /// 9. Hozzon létre animal.txt néven egy UTF-8 kódolású szöveges állományt 
+        ///    tartalmazza az összes animal(faction) adatait! 
+        ///    A fájlban a minta szerint szerepeljen kettősponttal elválasztva a kártya
+        ///    ereje(power) neve(name) faja(race) kasztja(class)! 
+        ///    A sorok erő szerint növekvő rendben legyenek az állományban!
+        /// </summary>
+        static void Feladat_09()
+        {
+            using (StreamWriter sw = new StreamWriter("animal.txt"))
+            {
+                foreach (Fantasycard item in fantasycards.FindAll(a => a.Faction.Equals("Animal")).OrderBy(b => b.Power))
+                {
+                    sw.WriteLine(string.Join(";", item.Power, item.Name, item.Race, item.Kaszt));
+                }
+            }
         }
     }
 }
