0e43998f3237d78afa360b47f329ce1fc4166376
borossandor27 <boros.sanyi@gmail.com>
2020-02-27 19:17:27 +0100

    1.0

---
+++ Fantasycard/Fantasycard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fantasycard
+{
+    class Fantasycard
+    {
+        int power;
+        string name;
+        string faction;
+        string race;
+        string osztaly;
+
+        public int Power { get => power; set => power = value; }
+        public string Name { get => name; set => name = value; }
+        public string Faction { get => faction; set => faction = value; }
+        public string Race { get => race; set => race = value; }
+        public string Osztaly { get => osztaly; set => osztaly = value; }
+
+        public Fantasycard(int power, string name, string faction, string race, string osztaly)
+        {
+            Power = power;
+            Name = name;
+            Faction = faction;
+            Race = race;
+            Osztaly = osztaly;
+        }
+    }
+}
--- Fantasycard/Program.cs
+++ Fantasycard/Program.cs
@@ -3,13 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Fantasycard
 {
     class Program
     {
+        static List<Fantasycard> fantasycards = new List<Fantasycard>();
+
         static void Main(string[] args)
         {
+            Beolvas(@"..\..\fantasycard.csv");
+            Feladat_03();
+            Console.WriteLine("Program vége!");
+            Console.ReadKey();
+        }
+        static void Beolvas(string forras)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(forras))
+                {
+                    sr.ReadLine();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] sor = sr.ReadLine().Split(';');
+                        fantasycards.Add(new Fantasycard(int.Parse(sor[0]), sor[1], sor[2], sor[3], sor[4]));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+        }
+
+        /// <summary>
+        /// 3. Határozza meg és írja ki a képernyőre a minta szerint, 
+        ///    hogy Leeroy (name) milyen faj (race) 
+        /// </summary>
+        static void Feladat_03()
+        {
+            Console.WriteLine($"\n3. feladat: {fantasycards.Find(a => a.Name.Equals("Leeroy")).Race}");
         }
     }
 }
